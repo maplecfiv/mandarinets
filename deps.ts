419b439be6187427d7d0bbb92d6ae11f7b0ef363
maplecfiv <maplecfiv@gmail.com>
2022-02-25 00:54:11 +0800

    error from outdated oak

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 // Copyright 2020-2020 The Mandarine.TS Framework authors. All rights reserved. MIT license.
 
 // @ts-ignore
-export { Application, Cookies, Request, Router, Context, Middleware, isHttpError, Status, FormDataReader, send, Response, ListenOptionsTls, ListenOptionsBase } from "https://raw.githubusercontent.com/mandarineorg/mandarinets-modules/master/oak/7.3.0/mod.ts";
+export { Application, Cookies, Request, Router, Context, Middleware, isHttpError, Status, FormDataReader, send, Response, ListenOptionsTls, ListenOptionsBase } from "https://deno.land/x/oak@v10.4.0/mod.ts";
 export { Leaf } from "https://deno.land/x/leaf@v1.0.3/mod.ts";
